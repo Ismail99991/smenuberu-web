bf7b442853df60d08d7f077119c70a071b9d1689
Ismail99991 <zairovismael@gmail.com>
2026-05-25 21:46:32 +0300

    Fix formatting issues in sort-filter-modal.tsx

--- components/sort-filter-modal.tsx
+++ components/sort-filter-modal.tsx
@@ -92,6 +92,7 @@ export default function SortFilterModal({
 
       <div className="absolute left-1/2 top-1/2 w-[min(560px,calc(100%-32px))] -translate-x-1/2 -translate-y-1/2">
         <div
+          data-open="true"
           className={cn(
             uiCard,
             uiModal,
