40e9d9fb3218298ab00bab5eb8daf1e117e8bed4
Ismail99991 <zairovismael@gmail.com>
2026-05-25 15:59:52 +0300

    Update ui.ts

--- lib/ui.ts
+++ lib/ui.ts
@@ -104,7 +104,6 @@ export const uiOverlay =
  * Контент модалки / popover
  */
 export const uiModal =
-export const uiModal =
   "transition-all duration-200 ease-out opacity-0 translate-y-2 data-[open=true]:opacity-100 data-[open=true]:translate-y-0";
 
 /**
