d6ec57c1320b2a7526f3b7378077a13f11613740
Ismail99991 <zairovismael@gmail.com>
2026-06-08 17:54:32 +0300

    Adjust bottom padding in shifts-client component

--- app/shifts/shifts-client.tsx
+++ app/shifts/shifts-client.tsx
@@ -444,7 +444,7 @@ export default function ShiftsClient() {
               className="
                 absolute
                 inset-x-0
-                bottom-[calc(40px+env(safe-area-inset-bottom))]
+                bottom-[calc(35px+env(safe-area-inset-bottom))]
                 max-h-[76vh]
                 rounded-t-[28px]
                 bg-white
